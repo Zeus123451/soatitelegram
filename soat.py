5519426691e311fcc0687a8797f58311f69c5476
Zeus123451 <66661041+Zeus123451@users.noreply.github.com>
2020-06-09 11:36:32 +0500

    Update soat.py

--- soat.py
+++ soat.py
@@ -4,18 +4,19 @@ import datetime
 import time
 import pytz
 import random
+import random
+chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+number = 1
+length = 50
+number = int(number)
+length = int(length)
+for n in range(number):
+    password =''
+    for i in range(length):
+        password += random.choice(chars)
 api_id = 1070048 #my.telegram.org dan olgan apiidni qoying
 api_hash = "d2c092dcc7d25a9e704cf4f11f0f58b0" #my.telegram.org dan olgan apihash ni qoying
 app = Client("my_account",api_id,api_hash)
-    chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-    number = 1
-    length = 10
-    number = int(number)
-    length = int(length)
-    for n in range(number):
-        password =''
-        for i in range(length):
-          password += random.choice(chars)
 
 app.start()
 while True:
